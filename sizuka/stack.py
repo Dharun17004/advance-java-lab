e3cc23a31b1c6e2496b7ab4acd2787950a078399
Dharun17004 <dharun.t2004@gmail.com>
2024-12-28 10:41:54 +0530

    Add files via upload

---
+++ sizuka/stack.py
@@ -0,0 +1,23 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Pop from empty stack")
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from empty stack")
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+stack = Stack()
+stack.push(10)
+stack.push(20)
+print("Popped:", stack.pop())
